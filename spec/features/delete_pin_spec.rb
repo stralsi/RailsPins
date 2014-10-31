c543659db89342e6d2a8ee8dfc381b8f89476907
Silviu <silviu.straliciuc@gmail.com>
2014-11-01 00:01:03 +0200

    delete pin spec - pending

--- spec/features/delete_pin_spec.rb
+++ spec/features/delete_pin_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe "deleting a pin" do
-  it "does not show on the index page any more"
+  let(:show_pin_page){ShowPinPage.new}
+  let(:author){FactoryGirl.create(:user)}
+  let(:not_author){FactoryGirl.create(:user)}
+  let(:pin){FactoryGirl.create(:pin, user_id:author.id)}
+
+  context "when logged in as the pin author" do
+    before { show_pin_page.visit_page_as(author, pin) }
+
+    it "does not show on the index page any more", :js => true do
+      pending
+      #
+      # find( '#pin_delete' ).click
+      # page.driver.browser.switch_to.alert.accept
+      #
+      # index_page = IndexPage.new
+      #
+      # expect(index_page).to_not have_pin(pin)
+    end
+  end
 end
